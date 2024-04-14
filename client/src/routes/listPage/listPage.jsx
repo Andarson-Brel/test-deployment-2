d584275535b3bce5652655aca860e6f62da9f468
Daniel Andara Innocent <106690294+Andarson-Brel@users.noreply.github.com>
2024-04-14 10:47:50 +0100

    Update listPage.jsx

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -2,55 +2,44 @@ import "./listPage.scss";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
 import Map from "../../components/map/Map";
-import { Await, useLoaderData } from "react-router-dom";
 import { Suspense } from "react";
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+
 function ListPage() {
     const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://test-deployment-2-krcx.onrender.com/api/posts')
-      .then(response => {
-        setPosts(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching posts:', error);
-      });
-  }, []);
-  const data = useLoaderData();
-console.log(posts)
-  return (
-    <div className="listPage">
-      <div className="listContainer">
-        <div className="wrapper">
-          <Filter />
-          <Suspense fallback={<p>Loading...</p>}>
-            <Await
-              resolve={data.postResponse}
-              errorElement={<p>Error loading posts!</p>}
-            >
-              {(postResponse) =>
-                postResponse.data.map((post) => (
-                  <Card key={post.id} item={post} />
-                ))
-              }
-            </Await>
-          </Suspense>
+    useEffect(() => {
+        axios.get('https://test-deployment-2-krcx.onrender.com/api/posts')
+            .then(response => {
+                setPosts(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching posts:', error);
+            });
+    }, []);
+
+    return (
+        <div className="listPage">
+            <div className="listContainer">
+                <div className="wrapper">
+                    <Filter />
+                    <Suspense fallback={<p>Loading...</p>}>
+                        {/* Map over the posts state */}
+                        {posts.map(post => (
+                            <Card key={post.id} item={post} />
+                        ))}
+                    </Suspense>
+                </div>
+            </div>
+            <div className="mapContainer">
+                <Suspense fallback={<p>Loading...</p>}>
+                    {/* Pass the posts state to the Map component */}
+                    <Map items={posts} />
+                </Suspense>
+            </div>
         </div>
-      </div>
-      <div className="mapContainer">
-        <Suspense fallback={<p>Loading...</p>}>
-          <Await
-            resolve={data.postResponse}
-            errorElement={<p>Error loading posts!</p>}
-          >
-            {(postResponse) => <Map items={postResponse.data} />}
-          </Await>
-        </Suspense>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ListPage;
