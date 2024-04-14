200e05659f3dcb063f9c9e1f3ce1f587921f1503
Daniel Andara Innocent <106690294+Andarson-Brel@users.noreply.github.com>
2024-04-14 10:20:51 +0100

    Update app.js

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({ origin:"*", credentials: true }));
+app.use(cors({ origin:"https://main--test-estateapp.netlify.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
