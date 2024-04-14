ff575ab1f18946775b3d0ed4fd9b07958b3e1e75
Daniel Andara Innocent <106690294+Andarson-Brel@users.noreply.github.com>
2024-04-14 21:38:05 +0100

    fixed Page not found error

--- api/app.js
+++ api/app.js
@@ -10,7 +10,12 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: "https://main--test-estateapp.netlify.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
