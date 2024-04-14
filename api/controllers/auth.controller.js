1f0fac1690946130ba5187c06e2230fc6fbde4d3
Daniel Andara Innocent <106690294+Andarson-Brel@users.noreply.github.com>
2024-04-14 09:06:22 +0100

    updated server env

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -68,9 +68,9 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure:true,
-
+        secure: true,
         maxAge: age,
+        sameSite: "None",
       })
       .status(200)
       .json(userInfo);
